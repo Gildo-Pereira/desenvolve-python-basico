d49f8d953ab55959736a07c69527d003135c7267
Gildo-Pereira <gildo.pereira@pditabira.com>
2025-04-05 14:49:23 -0300

    Add files via upload
    aula2.2

---
+++ Modulo2/aula2_questao2.py
@@ -0,0 +1,10 @@
+#Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+#Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+#Some os valores da segunda e terceira variável e armazene em outra variável.
+#Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+
+result="o resultado é"
+valor1=10
+valor2=3.5
+soma=valor1+valor2
+print(result,valor1,valor2,soma,type(result),type(valor1),type(valor2),type(soma))
---
+++ Modulo2/aula2_questao4.py
@@ -0,0 +1,9 @@
+#Uma conta poupança foi aberta com um depósito de R$500,00. Esta conta é remunerada em 1% de juros ao mês. O código a seguir 
+#apresenta uma forma de implementação para calcular três meses de acúmulo de juros. Reescreva esse código usando apenas duas
+#variáveis.
+
+saldo=500
+novo_saldo=saldo*1.01
+novo_saldo=novo_saldo*1.01
+novo_saldo=novo_saldo*1.01
+print(novo_saldo)
